c4957ea017c258c13caa7facdf9ab414161bd0bb
Ilya-Nazimov <ilyanazimov@yandex.ru>
2021-02-22 18:12:14 +0300

    Вывод актуальных для категории размеров

--- app/Http/Controllers/AllSizeForSideBarController.php
+++ app/Http/Controllers/AllSizeForSideBarController.php
@@ -7,11 +7,46 @@ use DB;
 
 class AllSizeForSideBarController extends Controller
 {
-    public function index(Request $request) {
-        $dataSizes = DB::table('catalog_size')
-            ->join('products', 'catalog_size.product_id', '=', 'products.product_id')->select('sizes_number', 'products.product_id')
-            ->join('sizes', 'catalog_size.sizes_id', '=', 'sizes.sizes_id')
+    //на вход названия пола, категории и подкатегории
+    public function index(Request $request, $sexalias, $depalias, $catalias) {
+
+        $sexid = DB::table('sex')->select("sex_id")->where('sex_alias', '=', $sexalias)->value("sex_id"); //id пола
+
+        $catid = NULL;
+        $depid = NULL;
+
+        if ($catalias !== 'null') {
+            $catid = DB::table('categories')->select("categories_id")->where('categories_alias', '=', $catalias)->value("categories_id"); //id категории
+        }
+
+        if ($depalias !== 'null') {
+            $depid = DB::table('departments')->select("departments_id")->where('departments_alias', '=', $depalias)->value("departments_id"); //id подкатегории
+        }
+
+        //id всех товаров в нужном разделе
+        $prodData = DB::table('products')->select("product_id")
+            ->where('product_available', '=', 1)
+            ->where('sex_id', '=', $sexid)
+            ->where('categories_id', 'like', $catid)
+            ->where('departments_id', 'like', $depid)
             ->get();
-        return $dataSizes;
+
+        $actualsizes = array();
+        $sizeData = array();
+
+        //поиск всех размеров по товарам в разделе
+        foreach ($prodDara as $i) {
+            $sizeData = DB::table('catalog_size')->select("sizes_id")
+                ->where('product_id', '=', $i)
+                ->get();
+            foreach ($sizeData as $j) {
+                array_push($actualsizes, $j);
+            }
+        }
+
+        //удаление дубликатов
+        $result = array_unique($actualsizes);
+
+        return $result;
     }
 }
